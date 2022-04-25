0ffcf0fc59bdd33da2ff257c184ca4fcca6d91be
Jeff Lin <thejefflin@gmail.com>
2022-04-24 23:24:34 -0400

    commit tree 6dea2613e4c00edf68829c062fa56f0ac49fe240

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+const body = document.getElementsByTagName("body") [0];
+
+function submitData(firstName, email) {
+    const configObject = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", configObject)
+    .then(response => response.json())
+    .then(body => addNewContact(body.id))
+    .catch(response => document.body.innerHTML = "Unauthorized Access")
+}
+function addNewContact(newID) {
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+}
+submitData("Name", "name@name.com");
